d1f068b14ea065ae6941f8458e5e4439c03b31e1
Jose Rodrigues da Costa <jose@skrip.dev>
2025-03-12 10:45:46 -0300

    Update chatwoot.js

--- src/chatwoot.js
+++ src/chatwoot.js
@@ -1,5 +1,32 @@
 async function loadAndExecuteScript() {
-  //
+  const scriptId = 'chatwoot_skrip_dev';
+  const scriptUrl = 'https://raw.githubusercontent.com/skrip-dev/code-injection/refs/heads/main/src/chatwoot.js';
+
+  if (document.querySelector(`script[id="${scriptId}"]`)) {
+    console.log('Script já foi carregado.');
+    return;
+  }
+
+  try {
+    const response = await fetch(scriptUrl);
+
+    if (!response.ok) {
+      throw new Error('Erro ao baixar o script: ' + response.statusText);
+    }
+
+    const scriptContent = await response.text();
+
+    const scriptElement = document.createElement('script');
+    scriptElement.type = 'text/javascript';
+    scriptElement.text = scriptContent;
+    scriptElement.id = scriptId;
+
+    document.body.appendChild(scriptElement);
+
+    console.log('Script carregado e executado com sucesso.');
+  } catch (error) {
+    console.error('Ocorreu um erro:', error);
+  }
 }
 
 loadAndExecuteScript();
