d865a4cfdc3fae3afceaf6863e48c87f4e07a889
Jose Rodrigues da Costa <jose@skrip.dev>
2025-03-12 11:23:09 -0300

    Update chatwoot_for_inject.js

--- src/chatwoot_for_inject.js
+++ src/chatwoot_for_inject.js
@@ -1,14 +1,69 @@
-console.log("skrip.dev console injected from downloaded file");
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
+  return null;
+}
 
-(async () => {
-	console.log("skrip.dev async function injected");
+function hideElements() {
+  const selectors = [
+    'li[name="Settings Inboxes"]',
+    'li[name="Settings Agent Bots"]',
+    'li[name="Settings Automation"]',
+    'li[name="Settings Macros"]',
+    'li[name="Settings Integrations"]',
+    'li[name="Reports Bot"]',
+    'div[name="Portals"]',
+    'div[name="Campaigns"]'
+  ];
 
-	await new Promise((resolve) => {
-		setTimeout(() => {
-			console.log("skrip.dev async function resolved");
-			resolve();
-		}, 10000);
-	});
+  selectors.forEach(function(selector) {
+    document.querySelectorAll(selector).forEach(function(element) {
+      if (element.tagName === 'DIV') {
+        const liElement = element.closest('li');
+        if (liElement) {
+          liElement.style.display = 'none';
+        }
+      } else {
+        element.style.display = 'none';
+      }
+    });
+  });
+}
 
-	console.log("skrip.dev async function resolved 10 seconds later");
-})();
+function watchPathChange() {
+  hideElements();
+
+  window.addEventListener('popstate', function() {
+    hideElements();
+  });
+
+  const pushState = history.pushState;
+  history.pushState = function() {
+    pushState.apply(history, arguments);
+    hideElements();
+  };
+
+  const replaceState = history.replaceState;
+  history.replaceState = function() {
+    replaceState.apply(history, arguments);
+    hideElements();
+  };
+}
+
+function execInjection() {
+  const cookieValue = getCookie('cw_d_session_info');
+  if (!cookieValue) {
+    return;
+  }
+
+  const cookieParsed = JSON.parse(decodeURIComponent(cookieValue))
+
+  if (cookieParsed.uuid.endsWith("@skrip.dev")) {
+    return;
+  }
+
+  watchPathChange();
+}
+
+execInjection();
