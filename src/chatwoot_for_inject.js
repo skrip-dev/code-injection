e5c145ceb7f61c35d57ffc4131567bbc9bb24f4c
Jose Rodrigues da Costa <jose@skrip.dev>
2025-03-13 09:13:03 -0300

    Update chatwoot_for_inject.js

--- src/chatwoot_for_inject.js
+++ src/chatwoot_for_inject.js
@@ -1,4 +1,6 @@
 function getCookie(name) {
+  console.log('Executando getCookie');
+
   const value = `; ${document.cookie}`;
   const parts = value.split(`; ${name}=`);
   if (parts.length === 2) return parts.pop().split(';').shift();
